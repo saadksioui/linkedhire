126e4ad1462de6477bd5b838de60ef7523f79759
Saad <saadksioui@gmail.com>
2025-03-11 23:17:37 +0000

    added the get and post methods

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -1,20 +1,46 @@
-export async function GET(req: Request) {
-  try {
-    const res = await fetch("https://www.arbeitnow.com/api/job-board-api");
+import { connectToDB } from "@/config/db";
+import { Job } from "@/models/job";
+import { NextResponse } from "next/server";
 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch jobs: ${res.status}`);
+export async function GET() {
+  try {
+    await connectToDB();
+    const jobs = await Job.find();
+
+    if (jobs.length === 0) {
+      return NextResponse.json({ message: "No jobs available" }, { status: 200 });
     }
 
-    const data = await res.json();
-    return new Response(JSON.stringify(data), {
-      headers: { "Content-Type": "application/json" },
-      status: 200,
-    });
+    return NextResponse.json(jobs, { status: 200 });
   } catch (error) {
-    return new Response(
-      JSON.stringify({ error: (error as Error).message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
+    console.error("Error fetching jobs:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+
+export async function POST(req: Request) {
+  try {
+    await connectToDB();
+    const body = await req.json()
+
+
+    if (!body.jobTitle || !body.companyName || !body.url) {
+      return NextResponse.json(
+        { error: "Missing required fields: jobTitle, companyName, and url" },
+        { status: 400 }
+      );
+    }
+
+    const newJob = new Job(body);
+    await newJob.save();
+
+    return NextResponse.json(
+      { message: "Job added successfully", job: newJob },
+      { status: 201 }
     );
+  } catch (error) {
+    console.error("Error adding job:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
